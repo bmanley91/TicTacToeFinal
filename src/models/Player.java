30e3a823adf3da33f21bb13bcb75d4017a9411d5
bmanley91 <brian.manley91@gmail.com>
2014-03-11 15:54:17 -0400

    Added methods to track wins, losses, draws

--- src/models/Player.java
+++ src/models/Player.java
@@ -11,6 +11,7 @@ public class Player {
 	public String name;
 	public String username;
 	private String password;
+	public int wins, losses, draws;
 	
 	public Player(String name, String password, String username) {
 		
@@ -51,5 +52,27 @@ public class Player {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+	
+	/* Methods to return number of wins, losses, draws -Brian */
+	public int getWins(){
+		return this.wins;
+	}
+	public int getLoss(){
+		return this.losses;
+	}
+	public int getDraw(){
+		return this.draws;
+	}
+	
+	/* Methods to  add wins, losses, draws -Brian */
+	public void addWin(){
+		this.wins++;
+	}
+	public void addLoss(){
+		this.losses++;
+	}
+	public void addDraw(){
+		this.draws++;
 	}
 }
